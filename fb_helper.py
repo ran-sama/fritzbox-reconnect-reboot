d40c1db49188a15023a36c47a1d175a8131e8995
Yakumo Ran <31311154+ran-sama@users.noreply.github.com>
2023-07-08 17:08:21 +0200

    Update fb_helper.py

--- fb_helper.py
+++ fb_helper.py
@@ -5,28 +5,28 @@ import xml.etree.ElementTree as ET
 
 def main():
     if len(sys.argv) != 5:
-        print(f"Usage: {sys.argv[0]} IP username password")
+        print(f"Usage: {sys.argv[0]} IP username password task")
         exit(1)
     url = "http://" + sys.argv[1] + "/login_sid.lua?version=2"
-    user = sys.argv[2]
-    pwd = sys.argv[3]
+    username = sys.argv[2]
+    password = sys.argv[3]
     my_task = sys.argv[4]
-    get_xml = requests.get(url).content
-    clean_xml = ET.fromstring(get_xml)
-    challenge = clean_xml.find("Challenge").text
-    tokens = challenge.split("$")
-    iterations_1 = int(tokens[1])
-    salt_1 = bytes.fromhex(tokens[2])
-    iterations_2 = int(tokens[3])
-    salt_2 = bytes.fromhex(tokens[4])
-    hash_1 = hashlib.pbkdf2_hmac("sha256", pwd.encode(), salt_1, iterations_1)
-    hash_2 = hashlib.pbkdf2_hmac("sha256", hash_1, salt_2, iterations_2)
-    response = f"{tokens[4]}${hash_2.hex()}"
-    data = {"username": user, "response": response}
+    r = requests.get(url)
+    xml = ET.fromstring(r.content)
+    challenge = xml.find("Challenge").text
+    challenge_parts = challenge.split("$")
+    iter1 = int(challenge_parts[1])
+    salt1 = bytes.fromhex(challenge_parts[2])
+    iter2 = int(challenge_parts[3])
+    salt2 = bytes.fromhex(challenge_parts[4])
+    hash1 = hashlib.pbkdf2_hmac("sha256", password.encode(), salt1, iter1)
+    hash2 = hashlib.pbkdf2_hmac("sha256", hash1, salt2, iter2)
+    challenge_response = "{}${}".format(challenge_parts[4], hash2.hex())
+    post_data = {"username": username, "response": challenge_response}
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
-    auth_response = requests.post(url,data,headers).content
-    get_xml = ET.fromstring(auth_response)
-    sid = get_xml.find("SID").text
+    r = requests.post(url, headers=headers, data=post_data)
+    xml = ET.fromstring(r.content) 
+    sid = xml.find("SID").text
     print(sid)
     print(my_task + " starting")
     if my_task == "reboot":
